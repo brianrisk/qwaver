e04a6c24a3321e5419b56b0a3b6a2c66bb07e4de
Brian Risk <brian@geneffects.com>
2022-10-20 17:15:27 -0400

    test

--- qwaver/settings.py
+++ qwaver/settings.py
@@ -68,7 +68,7 @@ DEFAULT_APPS = [
 
 THIRD_PARTY_APPS = [
     'crispy_forms',
-    'social_django'
+    # 'social_django'
 ]
 
 LOCAL_APPS = [
@@ -104,8 +104,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
+                # 'social_django.context_processors.backends',
+                # 'social_django.context_processors.login_redirect',
             ],
         },
     },
