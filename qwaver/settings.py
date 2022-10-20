4e800412e08c4616986ab207a768e46ac63e57dd
Brian Risk <brian@geneffects.com>
2022-10-20 07:23:08 -0400

    testing

--- qwaver/settings.py
+++ qwaver/settings.py
@@ -88,7 +88,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
+    # 'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'qwaver.urls'
